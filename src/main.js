85e9f80fafbf32a608fd2ebbe172cab6ffdd637a
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-20 21:20:38 +0200

    Added fontawesome by NPM modules. Import files on main.js and try an icon. it works without issues

--- src/main.js
+++ src/main.js
@@ -5,7 +5,15 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 const bootstrap = require('bootstrap')
 Vue.use(bootstrap)
-
+/* Import di Font Awesome */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+/* Add icons to the library */
+library.add(faUserSecret, faTwitter)
+/* Add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
